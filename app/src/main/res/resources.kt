ec946e47300c3e056a1edf2c70821a934d570f98
unseenMonsta93 <unseenMonsta945@gmail.com>
2019-08-02 08:38:32 -0500

    first commit

---
+++ app/src/main/res/resources.kt
@@ -0,0 +1,5 @@
+<resources>
+        <style name="AppTheme" parent="Theme.AppCompat.Light.DarkActionBar">
+        <item name="colorPrimaryDark">@color/colorPrimary</item>
+        <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+        <item name="colorAccent">@color/colorAccent</item>
